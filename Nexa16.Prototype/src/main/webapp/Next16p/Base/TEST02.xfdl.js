2344f149c6ef51850d5bd1ec8517573f5a26aece
Park SeongMin <Park SeongMin@soojeongB-PC>
2016-08-12 11:21:59 +0900

    Change-Id: If25a2073e93a2ef55d5b09841e4e997d2866069b Signed-off-by: Park SeongMin <Park SeongMin@soojeongB-PC>

--- Nexa16.Prototype/src/main/webapp/Next16p/Base/TEST02.xfdl.js
+++ Nexa16.Prototype/src/main/webapp/Next16p/Base/TEST02.xfdl.js
@@ -20,7 +20,38 @@
 
             
             // UI Components Initialize
+            obj = new Menu("Menu00","absolute","19","15","374","82",null,null,this);
+            obj.set_taborder("0");
+            this.addChild(obj.name, obj);
 
+            obj = new Spin("Spin00","absolute","15","107","424","65",null,null,this);
+            obj._setPropInitFunc(function (obj)
+            {
+                obj.set_taborder("1");
+            });
+            this.addChild(obj.name, obj);
+
+            obj = new ListBox("ListBox00","absolute","22","183","477","131",null,null,this);
+            obj._setPropInitFunc(function (obj)
+            {
+                obj.set_taborder("2");
+            });
+            this.addChild(obj.name, obj);
+
+            obj = new CheckBox("CheckBox00","absolute","406","20","168","65",null,null,this);
+            obj._setPropInitFunc(function (obj)
+            {
+                obj.set_taborder("3");
+                obj.set_text("CheckBox00");
+            });
+            this.addChild(obj.name, obj);
+
+            obj = new Radio("Radio00","absolute","475","115","104","57",null,null,this);
+            obj._setPropInitFunc(function (obj)
+            {
+                obj.set_taborder("4");
+            });
+            this.addChild(obj.name, obj);
             
             // Positionbase Setting
 
