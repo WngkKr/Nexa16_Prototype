313176ad212efbdd31e5af486ef7cda39573ea8c
Park SeongMin <Park SeongMin@soojeongB-PC>
2016-08-12 11:17:09 +0900

    Change-Id: I638381d260063e5baaa8b73dda69ddfeb5813bd4 Signed-off-by: Park SeongMin <Park SeongMin@soojeongB-PC>

---
+++ Nexa16.Prototype/src/main/webapp/Next16p/Base/TEST00.xfdl.js
@@ -0,0 +1,64 @@
+﻿(function()
+{
+    return function()
+    {
+        if (!this._is_form)
+            return;
+        
+        var obj = null;
+        
+        this.on_create = function()
+        {
+            if (Form == this.constructor)
+            {
+                this.set_name("TEST00");
+                this.set_titletext("New Form");
+                this._setFormPosition(0,0,1024,768);
+            }
+            
+            // Object(Dataset, ExcelExportObject) Initialize
+
+            
+            // UI Components Initialize
+            obj = new Button("Button00","absolute","20","23","462","169",null,null,this);
+            obj._setPropInitFunc(function (obj)
+            {
+                obj.set_taborder("0");
+                obj.set_text("TEST");
+            });
+            this.addChild(obj.name, obj);
+            
+            // Positionbase Setting
+
+
+            // Layout Functions
+            //-- Default Layout : this
+            obj = new Layout("default","",1024,768,this,function(p){});
+            this.addLayout(obj.name, obj);
+            
+            // BindItem Information
+
+        };
+        
+        this.loadPreloadList = function()
+        {
+
+        };
+        
+        // User Script
+
+        
+        // Regist UI Components Event
+        this.on_initEvent = function()
+        {
+
+        };
+
+        this.loadIncludeScript("TEST00.xfdl");
+        this.loadPreloadList();
+        
+        // Remove Reference
+        obj = null;
+    };
+}
+)();
---
+++ Nexa16.Prototype/src/main/webapp/Next16p/Base/TEST01.xfdl.js
@@ -0,0 +1,65 @@
+﻿(function()
+{
+    return function()
+    {
+        if (!this._is_form)
+            return;
+        
+        var obj = null;
+        
+        this.on_create = function()
+        {
+            if (Form == this.constructor)
+            {
+                this.set_name("TEST01");
+                this.set_titletext("New Form");
+                this._setFormPosition(0,0,1078,810);
+            }
+            
+            // Object(Dataset, ExcelExportObject) Initialize
+
+            
+            // UI Components Initialize
+            obj = new ImageViewer("ImageViewer00","absolute","16","8","1056","600",null,null,this);
+            obj._setPropInitFunc(function (obj)
+            {
+                obj.set_taborder("1");
+                obj.set_text("");
+                obj.set_image("URL(\"../../../../../../../Desktop/oncellClickEvent.png\")");
+            });
+            this.addChild(obj.name, obj);
+            
+            // Positionbase Setting
+
+
+            // Layout Functions
+            //-- Default Layout : this
+            obj = new Layout("default","",1078,810,this,function(p){});
+            this.addLayout(obj.name, obj);
+            
+            // BindItem Information
+
+        };
+        
+        this.loadPreloadList = function()
+        {
+
+        };
+        
+        // User Script
+
+        
+        // Regist UI Components Event
+        this.on_initEvent = function()
+        {
+
+        };
+
+        this.loadIncludeScript("TEST01.xfdl");
+        this.loadPreloadList();
+        
+        // Remove Reference
+        obj = null;
+    };
+}
+)();
